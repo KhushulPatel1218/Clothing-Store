8e5fbda50bc8b6cec587cfab7029cea180eb3e62
khushul <kpat8178@gmail.com>
2022-05-04 14:30:02 -0400

    2nd commit

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const router = express.Router();
+
 // sendFile will go here
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function(req, res) {
+app.get('/index', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/mens.html', function(req, res) {
+  res.sendFile(path.join(__dirname, '/mens.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/womens.html', function(req, res) {
+  res.sendFile(path.join(__dirname, '/womens.html'));
+});
+
+
 
 
 app.listen(port);
